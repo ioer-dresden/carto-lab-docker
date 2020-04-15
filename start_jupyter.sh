ea86c5d04ff5db320aee8d3f9548174fb141f03f
Marc Löchner <marc.loechner@tu-dresden.de>
2020-04-15 19:25:07 +0200

    jupyter weburl

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# Initialize conda for shell interaction
 conda init bash
+
+# shellcheck disable=SC1090
 source ~/.bashrc
+
+# Activate a conda environment
 conda activate worker_env
+
+# configure jupyter web url if not yet configured
+[[ -z $JUPYTER_WEBURL ]] && JUPYTER_WEBURL="http://$HOSTNAME:8888"
+
+# Start JupyterLab
 jupyter lab \
     --ip=0.0.0.0 \
     --allow-root \
-    --NotebookApp.custom_display_url=https://jupyterlab.jacks.lbsn.vgiscience.org \
+    --NotebookApp.custom_display_url="$JUPYTER_WEBURL" \
     --NotebookApp.notebook_dir=/home/jovyan/work
