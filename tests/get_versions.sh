4b49aafcaab5a9633ef9cfe0192d46391f3b2684
AD <alexander.dunkel@tu-dresden.de>
2025-08-01 09:59:16 +0200

    chore: update matrix script

--- tests/get_versions.sh
+++ tests/get_versions.sh
@@ -64,12 +64,12 @@ printf "\njupyter_env Python \n\n"
 docker compose run jupyterlab /opt/conda/envs/jupyter_env/bin/python --version
 printf "\nMapnik (requires container tag :mapnik) \n\n"
 docker compose run jupyterlab /usr/bin/python3 -c "import mapnik;print(mapnik.paths.__all__)"
-# echo "\nR Version (requies container tag :rstudio) \n\n"
+# echo "\nR Version (requires container tag :rstudio) \n\n"
 # docker compose run jupyterlab ls /opt/conda/envs/r_env/bin/
 printf "\nConda version: \n\n"
 docker compose run jupyterlab conda --version
-printf "\nR Version (requies container tag :rstudio) \n\n"
-docker compose run jupyterlab bash -c "conda activate r_env; R"
+printf "\nR Version (requires container tag :rstudio) \n\n"
+docker compose run jupyterlab bash -c "conda activate r_env && Rscript -e 'cat(R.version.string, \"\\n\")'"
 printf "\nnodejs version \n\n"
 # docker compose run jupyterlab bash -c "conda init;conda activate jupyter_env; nodejs"
 docker compose run jupyterlab /opt/conda/envs/jupyter_env/bin/node --version
